efdee79f01e9333237b11be1b8d6efdb9e15e440
ABay23 <alejandrobay23@gmail.com>
2024-03-16 21:24:51 -0400

    New Assignment

--- challenges/Data_Structures.py
+++ challenges/Data_Structures.py
@@ -1,15 +1,21 @@
 
-x= 'from marquard@gmail.com'
-print(x[14:17])
-print(len('banana')*7)
-greet = 'Hello bob'
-print(greet.upper())
+# x= 'from marquard@gmail.com'
+# print(x[14:17])
+# print(len('banana')*7)
+# greet = 'Hello bob'
+# print(greet.upper())
 
 # 6.5 Write code using find() and string slicing (see section 6.10) 
 # to extract the number at the end of the line below. Convert the extracted value to a
 # floating point number and print it out.
 
-text = "X-DSPAM-Confidence:    0.8475"
-print(text.find('.'))
-a = text.find('.')
-print(float(text[a -1: a + 5]))
+# text = "X-DSPAM-Confidence:    0.8475"
+# print(text.find('.'))
+# a = text.find('.')
+# print(float(text[a -1: a + 5]))
+
+def new_program(a):
+    print(a)
+    
+text = "Dolphin"
+new_program(text)
