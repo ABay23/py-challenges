ef1a91d015f01274a8b02354156a889a2358dddb
ABay23 <alejandrobay23@gmail.com>
2024-03-16 21:03:26 -0400

    New challenge

--- challenges/Data_Structures.py
+++ challenges/Data_Structures.py
@@ -5,4 +5,11 @@ print(len('banana')*7)
 greet = 'Hello bob'
 print(greet.upper())
 
-# 6.5 Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. Convert the extracted value to a floating point number and print it out.
+# 6.5 Write code using find() and string slicing (see section 6.10) 
+# to extract the number at the end of the line below. Convert the extracted value to a
+# floating point number and print it out.
+
+text = "X-DSPAM-Confidence:    0.8475"
+print(text.find('.'))
+a = text.find('.')
+print(float(text[a -1: a + 5]))
